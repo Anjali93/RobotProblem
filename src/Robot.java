708c55c3965f384aef69f9bc1769680ace63aa90
Anjali Verma <verma.anjali.3333@gmail.com>
2017-04-26 00:27:36 +0530

    comment added

--- src/Robot.java
+++ src/Robot.java
@@ -7,13 +7,13 @@ public class Robot
         for (int i =0 ; i < 4 ; i++)
         {
             for (int j =0 ; j < 4 ; j++)
-                Grid[i][j] = 0;
+                Grid[i][j] = 0; //comment
         }
 
         SolveMaze(0,0);
 
         System.out.print(Count);
-        
+
     }
 
     private  static void SolveMaze(int x , int y)
